9111a4dc052587e816333e68e64c9ef0034f4a60
HyeonWoo Park <gogoadl@naver.com>
2025-06-30 13:26:56 +0900

    Implement i18n resolver and global exception handling (#6)
    * Add i18n locale resolver and global exception handler
    
    * Set Korean as default locale
    
    * Use i18n and logging in global exception advice

---
+++ src/main/java/com/springboot/configuration/LocaleConfiguration.java
@@ -0,0 +1,18 @@
+package com.springboot.configuration;
+
+import com.springboot.utils.ProjectConstants;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+@Configuration
+public class LocaleConfiguration {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+        resolver.setDefaultLocale(ProjectConstants.DEFAULT_LOCALE);
+        return resolver;
+    }
+}
