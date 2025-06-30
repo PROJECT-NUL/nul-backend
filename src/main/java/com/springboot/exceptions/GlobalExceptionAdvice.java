6d644cd1c9ba9bb6d46081fb942530a9352fffa0
HyeonWoo Park <gogoadl@naver.com>
2025-06-30 13:26:28 +0900

    Use i18n and logging in global exception advice

--- src/main/java/com/springboot/exceptions/GlobalExceptionAdvice.java
+++ src/main/java/com/springboot/exceptions/GlobalExceptionAdvice.java
@@ -1,5 +1,8 @@
 package com.springboot.exceptions;
 
+import com.springboot.utils.ExceptionMessageAccessor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,13 +10,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionAdvice {
+
+    private static final String INTERNAL_SERVER_ERROR = "internal_server_error";
+
+    private final ExceptionMessageAccessor exceptionMessageAccessor;
 
     @ExceptionHandler(Exception.class)
     ResponseEntity<ApiExceptionResponse> handleAllExceptions(Exception exception) {
+        log.error("Unhandled exception", exception);
+        String message = exceptionMessageAccessor.getMessage(null, INTERNAL_SERVER_ERROR);
         ApiExceptionResponse response = new ApiExceptionResponse(
-                exception.getMessage(),
+                message,
                 HttpStatus.INTERNAL_SERVER_ERROR,
                 LocalDateTime.now()
         );
