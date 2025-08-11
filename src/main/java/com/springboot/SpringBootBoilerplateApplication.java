b23bcffe8e56d878a7d2de4e738a153ea40d0ba5
HyeonWoo Park <gogoadl@naver.com>
2025-08-11 17:12:35 +0900

    Add JpaAuditAware to provide auditor

--- src/main/java/com/springboot/SpringBootBoilerplateApplication.java
+++ src/main/java/com/springboot/SpringBootBoilerplateApplication.java
@@ -3,10 +3,12 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableJpaAuditing(auditorAwareRef = "jpaAuditAware")
 @EnableAspectJAutoProxy
 public class SpringBootBoilerplateApplication {
 
